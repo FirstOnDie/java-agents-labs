61ad4ed265b3c8141b35bc0aba85058195a4befd
Carlos <carlos.cjec@gmail.com>
2025-08-09 00:20:47 +0200

    Start project

---
+++ src/main/java/org/cexposito/javaagentslabs/JavaAgentsLabsApplication.java
@@ -0,0 +1,13 @@
+package org.cexposito.javaagentslabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAgentsLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAgentsLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cexposito/javaagentslabs/JavaAgentsLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cexposito.javaagentslabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAgentsLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
